e63fc0d7c5af4c4911ee629ffd60e41153a3ff88
ninemia123 <hagistamatis@protonmail.com>
2026-05-02 12:46:58 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,41 @@
-// ΑΥΤΟΜΑΤΗ ΕΚΔΟΣΗ - Διάβασε την έκδοση από το HTML
-const CACHE_NAME = "imerast-v" + new URL(location.href).searchParams.get('v') || "7.4";
+
+    const version = new URL(location.href).searchParams.get('v') || "7.4";
+const CACHE_NAME = "imerast-v" + version;
+
 const assets = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./assets/icon-192.png",
-  "./assets/icon-512.png"
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
-  console.log("Service Worker installing...");
-  self.skipWaiting(); // Force activate νέο SW
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
   );
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker activating...");
-  // Διέγραψε παλιές caches
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log("Deleting old cache:", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
   );
-  // Ανέλαβε έλεγχο όλων των tabs
   event.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Network first για HTML, cache first για assets
-      if (event.request.mode === 'navigate') {
-        return fetch(event.request).catch(() => response);
-      }
-      return response || fetch(event.request);
-    })
-  );
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match("/index.html"))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((res) => res || fetch(event.request))
+    );
+  }
 });
